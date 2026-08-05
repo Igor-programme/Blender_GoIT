60e99a458df92e460f47be0b92d065ef8cbacee9
Igor-programme <snabteploenergo@gmail.com>
2026-08-05 21:24:43 +0300

    Додано імпорт бібліотек

---
+++ Python/libraries.py
@@ -0,0 +1,9 @@
+#Імпортуємо всі бібліотеки для роботи
+#Робота з таблицями (dataFrame)
+import pandas as pd
+#Робота з математикою масивами
+import numpy as np
+#Базова бібліотека для побудови графіків
+import matplotlib.pyplot as plt
+#Побудова над matplotlib для красивих і статистичних графіків
+import seaborn as sns
